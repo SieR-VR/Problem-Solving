2c6530bd62381a448ba9c411da47a17b62e466c2
SieR-VR <nwh040512@gmail.com>
2024-06-03 19:08:05 +0900

    update script

--- new.sh
+++ new.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-PROBLEM_NUMBER=$(gum input --placeholder "Enter the problem number")
 
-mkdir $PROBLEM_NUMBER && touch $PROBLEM_NUMBER/$PROBLEM_NUMBER.c
+if [ -z "$1" ]; then
+  PROBLEM_NUMBER=$(gum input --placeholder "Enter the problem number")
+else
+  PROBLEM_NUMBER=$1
+fi
+
+mkdir $PROBLEM_NUMBER && cp templates/.c $PROBLEM_NUMBER/$PROBLEM_NUMBER.c
 
 code -v > /dev/null && code $PROBLEM_NUMBER/$PROBLEM_NUMBER.c
